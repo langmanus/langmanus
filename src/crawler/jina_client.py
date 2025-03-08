d62ad56083bfdd910a4eee830725879ab0bf8888
Henry Li <henry1943@163.com>
2025-03-08 15:19:53 +0800

    feat: use logger to warn

--- src/crawler/jina_client.py
+++ src/crawler/jina_client.py
@@ -1,11 +1,13 @@
+import logging
 import os
 
 import requests
 
+logger = logging.getLogger(__name__)
+
 
 class JinaClient:
     def crawl(self, url: str, return_format: str = "html") -> str:
-        url = "https://r.jina.ai/https://finance.sina.com.cn/stock/relnews/us/2024-08-15/doc-incitsya6536375.shtml"
         headers = {
             "Content-Type": "application/json",
             "X-Return-Format": return_format,
@@ -13,9 +15,8 @@ class JinaClient:
         if os.getenv("JINA_API_KEY"):
             headers["Authorization"] = f"Bearer {os.getenv('JINA_API_KEY')}"
         else:
-            print(
-                "WARNING: Jina API key is not set. Provide your own key to access a higher rate limit.",
-                "See https://jina.ai/reader for more information.",
+            logger.warning(
+                "Jina API key is not set. Provide your own key to access a higher rate limit. See https://jina.ai/reader for more information."
             )
         data = {"url": url}
         response = requests.post(url, headers=headers, json=data)
