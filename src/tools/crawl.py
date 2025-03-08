7adb492ae4f6acda9177a7d6e8c66cdfcb125b88
Henry Li <henry1943@163.com>
2025-03-08 15:59:44 +0800

    feat: return markdown directly

--- src/tools/crawl.py
+++ src/tools/crawl.py
@@ -12,12 +12,12 @@ logger = logging.getLogger(__name__)
 @tool
 def crawl_tool(
     url: Annotated[str, "The url to crawl."],
-) -> ToolMessage:
+) -> str:
     """Use this to crawl a url and get a readable content in markdown format."""
     try:
         crawler = Crawler()
         article = crawler.crawl(url)
-        return article.to_message()
+        return article.to_markdown()
     except BaseException as e:
         error_msg = f"Failed to crawl. Error: {repr(e)}"
         logger.error(error_msg)
