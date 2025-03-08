054ed4ccaa964fe48986632c5c368a6c9ddb9c14
Henry Li <henry1943@163.com>
2025-03-08 14:19:31 +0800

    chore: rename

--- src/crawler/readability_extractor.py
+++ src/crawler/readability_extractor.py
@@ -1,15 +1,10 @@
 from readabilipy import simple_json_from_html_string
 
-
-class Article:
-    def __init__(self, title: str, content: str, url: str):
-        self.title = title
-        self.content = content
-        self.url = url
+from .article import Article
 
 
-class ReadabilityParser:
-    def parse_article(self, html: str) -> Article:
+class ReadabilityExtractor:
+    def extract_article(self, html: str) -> Article:
         article = simple_json_from_html_string(html, use_readability=True)
         return Article(
             title=article.get("title"),
