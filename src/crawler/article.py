36f3528f6a6812d86094d128f5073c56296132bc
Henry Li <henry1943@163.com>
2025-03-08 14:28:51 +0800

    refactor: re-order parameters

--- src/crawler/article.py
+++ src/crawler/article.py
@@ -5,10 +5,10 @@ from markdownify import markdownify as md
 
 
 class Article:
-    def __init__(self, title: str, content: str, url: str):
+    def __init__(self, url: str, title: str, content: str):
+        self.url = url
         self.title = title
         self.content = content
-        self.url = url
 
     def to_markdown(self, including_title: bool = True) -> str:
         markdown = ""
