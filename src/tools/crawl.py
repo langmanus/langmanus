bf32231b6246f27e6037df9925d2a9350fd81151
Henry Li <henry1943@163.com>
2025-03-08 14:49:29 +0800

    fix: change method name

--- src/tools/crawl.py
+++ src/tools/crawl.py
@@ -16,7 +16,7 @@ def crawl_tool(
     try:
         crawler = Crawler()
         article = crawler.crawl(url)
-        return article.to_message()
+        return article.to_message_content()
     except BaseException as e:
         error_msg = f"Failed to crawl. Error: {repr(e)}"
         logger.error(error_msg)
