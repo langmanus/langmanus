e1c0cc626105925bcfc431c37b658d45d86811b9
Henry Li <henry1943@163.com>
2025-03-08 14:19:55 +0800

    feat: remove `app_key` from constructor parameters

--- src/crawler/jina_client.py
+++ src/crawler/jina_client.py
@@ -4,22 +4,18 @@ import requests
 
 
 class JinaClient:
-    def __init__(self, api_key: str = None):
-        self.api_key = api_key
-
     def crawl_html(self, url: str) -> str:
         url = "https://r.jina.ai/https://finance.sina.com.cn/stock/relnews/us/2024-08-15/doc-incitsya6536375.shtml"
         headers = {
             "Content-Type": "application/json",
             "X-Return-Format": "html",
         }
-        if self.api_key:
-            headers["Authorization"] = f"Bearer {self.api_key}"
-        elif os.getenv("JINA_API_KEY"):
+        if os.getenv("JINA_API_KEY"):
             headers["Authorization"] = f"Bearer {os.getenv('JINA_API_KEY')}"
         else:
             print(
-                "WARNING: Jina API key is not set. Provide your Jina API key to access a higher rate limit. "
+                "WARNING: Jina API key is not set. Provide your own key to access a higher rate limit.",
+                "See https://jina.ai/reader for more information.",
             )
         data = {"url": url}
         response = requests.post(url, headers=headers, json=data)
